576f67180c67d446f200f6363514382d727da5ce
Axi <axiprogrammer@gmail.com>
2021-05-26 13:08:53 +0300

    download pdf2

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  Stream<double?> getDouble()async*{
+    for(int i=0;i<=100;i++){
+      await Future.delayed(Duration(seconds: 2));
+      print(i.toDouble());
+      yield i.toDouble();
+    }
+  }
   @override
   void initState(){
     super.initState();
@@ -39,36 +45,73 @@ class _HomePageState extends State<HomePage> {
        body: Consumer<BioProvider>(
          builder: (context,value,child)=>Center(
            child:Center(
-             child:Column(
-               crossAxisAlignment: CrossAxisAlignment.center,
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 TextButton(
-                     onPressed: ()async{
-                       await value.downloadXXPDF();
-                     },
-                     child:Text("Download",style: TextStyle(color: Colors.orange),),
-                 ),
-                 SizedBox(height: 10,),
-                 Text(value.getProgress.toStringAsFixed(2)+"%"),
-                 SizedBox(height: 10,),
-                 LinearProgressIndicator(
-                   value:value.getProgress/100,
-                   minHeight: 20,
-                   valueColor: AlwaysStoppedAnimation<Color?>(
-                     Colors.red,
-                   ),
-                 ),
-                 SizedBox(height: 20,),
-                 CircularProgressIndicator(
-                   value: value.getProgress/100,
-                   backgroundColor: Colors.black,
-                   valueColor: AlwaysStoppedAnimation<Color?>(
-                     Colors.red,
-                   ),
-                 ),
-
-               ],
+             child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.center,
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     TextButton(
+                       onPressed: ()async{
+                         await value.downloadXXPDF();
+                       },
+                       child:Text("Download",style: TextStyle(color: Colors.orange),),
+                     ),
+                     SizedBox(height: 10,),
+                     Text(value.getProgress.toStringAsFixed(2)+"%"),
+                     SizedBox(height: 10,),
+                     LinearProgressIndicator(
+                       value:value.getProgress/100,
+                       minHeight: 20,
+                       valueColor: AlwaysStoppedAnimation<Color?>(
+                         Colors.red,
+                       ),
+                     ),
+                     SizedBox(height: 20,),
+                     CircularProgressIndicator(
+                       value: value.getProgress/100,
+                       backgroundColor: Colors.black,
+                       valueColor: AlwaysStoppedAnimation<Color?>(
+                         Colors.red,
+                       ),
+                     ),
+                     SizedBox(height: 20,),
+                     StreamBuilder<double?>(
+                       stream: getDouble(),
+                         builder:(context,snapshot){
+                           if(snapshot.hasData){
+                             return Column(
+                               crossAxisAlignment: CrossAxisAlignment.center,
+                               children: [
+                                 LinearProgressIndicator(
+                                   backgroundColor: Colors.black,
+                                   value: snapshot.data!/100,
+                                   valueColor: AlwaysStoppedAnimation<Color?>(
+                                     Colors.green,
+                                   ),
+                                 ),
+                                 SizedBox(height: 20,),
+                                 CircularProgressIndicator(
+                                   backgroundColor: Colors.black,
+                                   value: snapshot.data!/100,
+                                   valueColor: AlwaysStoppedAnimation<Color?>(
+                                     Colors.green,
+                                   ),
+                                 ),
+                                 SizedBox(height: 20,),
+                               ],
+                             );
+                           }else{
+                             return Text("kuting...");
+                           }
+                         }
+                     ),
+                     TextButton(
+                       child: Text("Stream",style: TextStyle(color: Colors.black),),
+                       onPressed: (){
+                         getDouble();
+                         print('pressed');
+                       },
+                     ),
+                   ],
              )
            ),
          ),
