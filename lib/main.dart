d6cd85c8bc8d3dfd7ae6666eb037b45ee1b224ab
Axi <axiprogrammer@gmail.com>
2021-05-26 12:41:31 +0300

    download pdf1

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,8 @@ class _HomePageState extends State<HomePage> {
                    valueColor: AlwaysStoppedAnimation<Color?>(
                      Colors.red,
                    ),
-                 )
+                 ),
+
                ],
              )
            ),
